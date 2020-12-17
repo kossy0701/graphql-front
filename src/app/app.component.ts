789c07b6773e163ec2885c0fda57d88d9520ecdb
kossy0701 <0421kossy0421@gmail.com>
2020-12-17 21:41:38 +0900

    run query in ngOnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'graphql-front';
+  posts = [];
+
+  constructor(private apolloClient: Apollo) {}
+
+  ngOnInit(): void {
+    this.apolloClient.watchQuery({
+      query: gql`
+        {
+          posts {
+            title
+            body
+          }
+        }
+      `
+    }).valueChanges.subscribe((result: any) => {
+      this.posts = result?.data;
+    });
+  }
 }
